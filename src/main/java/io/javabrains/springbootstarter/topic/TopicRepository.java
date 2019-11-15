d6a97750109c4d50a074ed9160ee35ae24251ee6
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-11-15 18:39:24 +0600

    Making Crud Operations with Repository

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,6 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+}
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,14 +1,16 @@
 package io.javabrains.springbootstarter.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 @Service
 public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
     private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Core Java", "Core Java Description"),
@@ -16,32 +18,40 @@ public class TopicService {
     ));
 
     public List<Topic> getTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id) {
-        try {
-            return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-        } catch (NoSuchElementException e) {
-            return new Topic(id, "NoSuchElementException", e.toString());
-        }
+//        try {
+//            return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+//        } catch (NoSuchElementException e) {
+//            return new Topic(id, "NoSuchElementException", e.toString());
+//        }
+
+        return topicRepository.findById(id).get();
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+//        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if(t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+//        for (int i = 0; i < topics.size(); i++) {
+//            Topic t = topics.get(i);
+//            if(t.getId().equals(id)) {
+//                topics.set(i, topic);
+//                return;
+//            }
+//        }
+
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+//        topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
